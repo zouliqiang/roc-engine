ab5596a1ef769729288dc95d5c5d66b92a99ae6d
zouliqiang <876880864@qq.com>
2020-06-13 14:26:52 +0800

    项目初始化提交

---
+++ roc-engine-meta/src/main/java/com/roc/engine/meta/MetaManager.java
@@ -0,0 +1,26 @@
+package com.roc.engine.meta;
+
+import java.util.List;
+import java.util.Map;
+
+import com.roc.engine.meta.domain.Region;
+import com.roc.engine.meta.domain.Table;
+
+/**
+ * @Description 元数据管理器
+ * @date  2020年6月13日下午1:33:57
+ * @version V1.0  
+ * @author 邹立强
+ * <p>Copyright (c) Department of Research and Development/Beijing.</p>
+ */
+public class MetaManager {
+    
+    private Map<String,Region> regions;
+    
+    private Map<String,Table> tables;
+    
+    public void initMeta(List<Region> region) {
+        
+    }
+    
+}
